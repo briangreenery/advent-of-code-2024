23a6b1b0f9ff577729a60d380b861096dade3cca
Brian Green <briangreenery@gmail.com>
2024-12-19 14:37:52 -0800

    cleanup

--- 19.js
+++ 19.js
@@ -4,25 +4,9 @@ let [a, b] = fs.readFileSync('input.txt').toString().trim().split('\n\n');
 let towels = a.split(', ');
 let lines = b.split('\n');
 
-function startsWith(pattern, i, towel) {
-    let size = pattern.length - i;
-
-    if (towel.length > size) {
-        return false;
-    }
-
-    for (let j = 0; j < towel.length; j += 1) {
-        if (pattern[i + j] !== towel[j]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 function count(pattern, i, memo) {
     if (i === pattern.length) {
-        return true;
+        return 1;
     }
 
     let cached = memo[i];
@@ -34,11 +18,9 @@ function count(pattern, i, memo) {
 
     for (let j = 0; j < towels.length; j += 1) {
         let towel = towels[j];
-        if (!startsWith(pattern, i, towel)) {
-            continue;
+        if (pattern.startsWith(towel, i)) {
+            ans += count(pattern, i + towel.length, memo);
         }
-
-        ans += count(pattern, i + towel.length, memo);
     }
 
     memo[i] = ans;
@@ -50,15 +32,14 @@ lines.forEach(line => max = Math.max(max, line.length));
 
 let memo = [];
 for (let i = 0; i < max; i += 1) {
-    memo[i] = -1;
+    memo.push(-1);
 }
 
 let sum = 0;
 let distinct = 0;
 
 lines.forEach(line => {
-    let result =  count(line, 0, memo);
-    
+    let result = count(line, 0, memo);
     if (result > 0) {
         sum += 1;
         distinct += result;
